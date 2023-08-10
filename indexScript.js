f0c5e15054b1da1663dacd24cf91ac1880a866d6
gom1n <alice43677@naver.com>
2023-08-10 17:12:20 +0900

    Design: 표지 > 각종 이미지 및 요소 추가
    제목 이미지 추가
    캐릭터 (테스트용) 추가 및 애니메이션 구현
    시작하기 버튼 커스텀
    접속자 카운트 라벨 추가
    뒷배경 박스 추가
    모나 로고 추가

--- indexScript.js
+++ indexScript.js
@@ -6,4 +6,19 @@ startButton.addEventListener('click', function () {
     window.location.href = 'question.html';
 });
 
-imageContainer.innerHTML = `<img src='./Images/demo.png' alt="Image">`;
+function animateImage() {
+    const test_character = document.getElementById("test_character");
+    
+    // 이미지 크기와 투명도를 변화시켜 애니메이션 효과 생성
+    test_character.style.width = "75%";
+    test_character.style.opacity = "1";
+    // 일정 시간 후 이미지 축소하는 애니메이션
+    setTimeout(function() {
+        test_character.style.width = "50%"; // 원하는 크기로 조정
+    }, 400); // 400ms(0.4초) 후에 축소 애니메이션 시작
+}
+
+// 페이지 로드 시 이미지 애니메이션 시작
+window.onload = function() {
+    animateImage();
+};
