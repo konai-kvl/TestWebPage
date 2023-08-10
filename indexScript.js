dcc515f85a2fc1be3b6f8b8aadc7eea2828dee44
gom1n <alice43677@naver.com>
2023-08-10 21:38:42 +0900

    Fix: 표지 화면 UI 변경 및 링크 공유하기 기능 추가

--- indexScript.js
+++ indexScript.js
@@ -1,20 +1,34 @@
 const startButton = document.getElementById('startButton');
-const imageContainer = document.getElementById('image-container');
+const link_share = document.getElementById('link_share');
 
-// 게임 시작하기 버튼
+// 테스트 시작하기 버튼 클릭 이벤트
 startButton.addEventListener('click', function () {
     window.location.href = 'question.html';
 });
+// 테스트 공유하기 버튼 클릭 이벤트
+link_share.addEventListener('click', function () {
+    const currentURL = window.location.href;
 
+  const tempInput = document.createElement("input");
+  tempInput.value = currentURL;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+
+  alert("링크가 복사되었습니다.");
+});
+
+// 이미지 애니메이션
 function animateImage() {
     const test_character = document.getElementById("test_character");
     
     // 이미지 크기와 투명도를 변화시켜 애니메이션 효과 생성
-    test_character.style.width = "75%";
+    test_character.style.width = "90%";
     test_character.style.opacity = "1";
     // 일정 시간 후 이미지 축소하는 애니메이션
     setTimeout(function() {
-        test_character.style.width = "50%"; // 원하는 크기로 조정
+        test_character.style.width = "65%"; // 원하는 크기로 조정
     }, 400); // 400ms(0.4초) 후에 축소 애니메이션 시작
 }
 
