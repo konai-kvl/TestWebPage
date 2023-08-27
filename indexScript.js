a8289dc26db69d0362de082971efdb1d861c37b4
gom1n <alice436777@gmail.com>
2023-08-27 10:23:02 +0900

    [file] 이미지 경로 레벨별 구분 및 적용

--- indexScript.js
+++ indexScript.js
@@ -20,18 +20,18 @@ link_share.addEventListener('click', function () {
 });
 
 startButton.addEventListener("mouseenter", () => {
-    startButton.src = "./Images/start_button_hover.png"; // 호버시 이미지 변경
+    startButton.src = "./Images/Start/start_button_hover.png"; // 호버시 이미지 변경
 });
   
   startButton.addEventListener("mouseleave", () => {
-    startButton.src = "./Images/start_button.png"; // 마우스가 벗어났을 때 이미지 원래대로 변경
+    startButton.src = "./Images/Start/start_button.png"; // 마우스가 벗어났을 때 이미지 원래대로 변경
 });
 link_share.addEventListener("mouseenter", () => {
-    link_share.src = "./Images/share_test_button_hover.png"; // 호버시 이미지 변경
+    link_share.src = "./Images/Start/share_test_button_hover.png"; // 호버시 이미지 변경
 });
   
 link_share.addEventListener("mouseleave", () => {
-    link_share.src = "./Images/share_test_button.png"; // 마우스가 벗어났을 때 이미지 원래대로 변경
+    link_share.src = "./Images/Start/share_test_button.png"; // 마우스가 벗어났을 때 이미지 원래대로 변경
 });
 
 
